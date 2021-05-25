586c81832f0d451c4639ca1d15d5f5284f1c325a
mizzypd441 <73121203+mizzypd441@users.noreply.github.com>
2021-05-25 19:45:54 +0100

    test

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'hfshowdown',
-	host: 'hfs-master.herokuapp.com.git',
+	host: 'hfs-master.herokuapp.com',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
